13a8a20b73ebbf152f66308b9bc21ee1f8d93e4c
unknown <akasiyapika@gmail.com>
2024-01-24 02:04:50 +0400

    Delete mvc

--- StackOverflow/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace StackOverflow.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
