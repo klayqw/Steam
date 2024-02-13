6c4cefc632f41d86d7d86bc6c304a3753e2d6151
unknown <akasiyapika@gmail.com>
2024-02-13 17:06:04 +0400

    Edit Sql File

--- Steam/SQL/SQLQuery_1.sql
+++ Steam/SQL/SQLQuery_1.sql
@@ -6,10 +6,19 @@ create table Logs(
     [Id] int primary key identity,
     [UserId] nvarchar(max),
     [Url] NVARCHAR(max),
-    [MethodType] nvarchar(max),
-    [StatusCode] nvarchar(max),
+    [MethodType] nvarchar(10),
+    [StatusCode] nvarchar(10),
     [RequestBody] NVARCHAR(max),
+    [ResposneBody] NVARCHAR(max)
 )
 
-ALTER TABLE Logs
-ADD ResponseBody nvarchar(max);
+
+create table Users(
+    [Id] int PRIMARY key IDENTITY,
+    [Login] NVARCHAR(20),
+    [Password] NVARCHAR(20),
+    [Email] NVARCHAR(50),
+)
+
+select * from Logs
+select * from Users
