88eebd5667d15fd34278183b462a9afc44df646c
unknown <akasiyapika@gmail.com>
2024-02-20 21:54:02 +0400

    Add Autho

---
+++ Steam/SQL/SQLQuery_1.sql
@@ -0,0 +1,39 @@
+create DATABASE Steam
+
+use Steam
+
+create table Logs(
+    [Id] int primary key identity,
+    [UserId] nvarchar(max),
+    [Url] NVARCHAR(max),
+    [MethodType] nvarchar(10),
+    [StatusCode] nvarchar(10),
+)
+
+create table Users(
+    [Id] int PRIMARY key IDENTITY,
+    [Login] NVARCHAR(20) not null,
+    [Password] NVARCHAR(20) not null,
+    [Email] NVARCHAR(50) not null,
+    [GameId] INT FOREIGN KEY REFERENCES Games(Id)
+)
+
+
+drop table Games
+
+create table Games(
+    [Id] int PRIMARY KEY IDENTITY,
+    [Title] NVARCHAR(50) not null,
+    [Description] NVARCHAR(max) not null,
+    [Devoloper] NVARCHAR(100) not null,
+    [Publisher] NVARCHAR(100) not null,
+    [Price] FLOAT not null,
+    [ReleaseDate] datetime not null,
+    [Genre] NVARCHAR(100) not null,
+)
+
+select * from Logs
+
+SELECT * from Games
+
+select * from Users
